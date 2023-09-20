b3abbfb56bb32cf6a3846bf7c48d8dcae9b0bb15
Asher  Manangan <51806917+asher-lab@users.noreply.github.com>
2023-09-20 13:57:31 +0800

    Create values.sql

---
+++ values.sql
@@ -0,0 +1,7 @@
+INSERT INTO SHIPMENTS (shipment_date, customer_name, product_name, quantity)
+VALUES
+   ('2023-09-23', 'Alice Johnson', 'Widget C', 30),
+   ('2023-09-24', 'David Smith', 'Widget A', 80),
+   ('2023-09-25', 'Emily Davis', 'Widget B', 60),
+   ('2023-09-26', 'Michael Brown', 'Widget C', 45),
+   ('2023-09-27', 'Olivia Wilson', 'Widget A', 55);
