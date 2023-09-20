7c89a2e248a51796711d4011273e92438e86f320
Asher  Manangan <51806917+asher-lab@users.noreply.github.com>
2023-09-20 14:00:32 +0800

    Update values.sql

--- values.sql
+++ values.sql
@@ -1,6 +1,6 @@
 INSERT INTO SHIPMENTS (shipment_date, customer_name, product_name, quantity)
 VALUES
-   ('2023-09-23', 'Alice Johnson', 'Widget C', 30),
+   ('2023-09-23', 'Alice Johnson', 'Widget D', 30),
    ('2023-09-24', 'David Smith', 'Widget A', 80),
    ('2023-09-25', 'Emily Davis', 'Widget B', 60),
    ('2023-09-26', 'Michael Brown', 'Widget C', 45),
